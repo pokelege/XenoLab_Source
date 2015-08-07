44c4b69b73f72ce0824ddbb948ac157e55941f7c
ポケレゲミッシングナムバー <pokelegemissingno@gmail.com>
2015-08-06 19:04:11 -0600

    set lock

--- ProjectTap/ConstrainingSpringArmComponent.h
+++ ProjectTap/ConstrainingSpringArmComponent.h
@@ -15,16 +15,19 @@ class PROJECTTAP_API UConstrainingSpringArmComponent : public USpringArmComponen
 
 protected:
 	FVector lastLockPosition;
+	bool lockX = false;
+	bool lockY = false;
+	bool lockZ = true;
 public:
-	UPROPERTY( EditAnywhere , BlueprintReadWrite , Category = Constraint )
-		bool lockX = false;
-	UPROPERTY( EditAnywhere , BlueprintReadWrite , Category = Constraint )
-		bool lockY = false;
-	UPROPERTY( EditAnywhere , BlueprintReadWrite , Category = Constraint )
-		bool lockZ = true;
 	void SetTargetOffsetCustom(const FVector& newTargetOffset);
 	void SetLockPosition(const FVector& lastLockPosition);
+	UFUNCTION(BlueprintCallable, Category = Lock)
+	void SetLockX(bool isXLock);
+	UFUNCTION(BlueprintCallable, Category = Lock)
+	void SetLockY(bool isYLock);
+	UFUNCTION(BlueprintCallable, Category = Lock)
+	void SetLockZ(bool isZLock);
 protected:
 	/** Updates the desired arm location, calling BlendLocations to do the actual blending if a trace is done */
-	virtual void UpdateDesiredArmLocation( bool bDoTrace , bool bDoLocationLag , bool bDoRotationLag , float DeltaTime );
+	virtual void UpdateDesiredArmLocation(bool bDoTrace, bool bDoLocationLag, bool bDoRotationLag, float DeltaTime);
 };
